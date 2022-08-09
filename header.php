3776df7aebae5f19675c0e846037ea1ac9444e33
Serhat ÇİÇEK <serhatdbs@outlook.com>
2022-08-10 01:26:30 +0300

    header

--- header.php
+++ header.php
@@ -1,30 +1,35 @@
+<div class="collapse bg-dark" id="navbarHeader">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 col-md-7 py-4">
+                <h4 class="text-white">About</h4>
+                <p class="text-muted">Vulnerable WebSocket Application.</p>
+            </div>
+            <div class="col-sm-4 offset-md-1 py-4">
+                <h4 class="text-white">Contact</h4>
+                <ul class="list-unstyled">
+                    <li><a href="https://www.linkedin.com/in/serhatcck/" target="_blank" class="text-white">Linkedin</a></li>
+                    <li><a href="https://github.com/Serhatcck" target="_blank" class="text-white">Github</a></li>
 
-<?php if (isset($_SESSION['is_admin']) && $_SESSION['is_admin']) { ?>
-    <div class="navbar navbar-dark bg-dark box-shadow">
-        <div class="container d-flex justify-content-between">
-            <a href="/vulnsocket" class="navbar-brand d-flex align-items-center">
-
-                <strong>Welcome Admin!</strong>
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+                </ul>
+            </div>
         </div>
     </div>
-<?php } else { ?>
+</div>
+<div class="navbar navbar-dark bg-dark box-shadow">
+    <div class="container d-flex justify-content-between">
+        <a href="#" class="navbar-brand d-flex align-items-center">
 
-    <div class="navbar navbar-dark bg-dark box-shadow">
-        <div class="container d-flex justify-content-between">
-            <a href="/vulnsocket" class="navbar-brand d-flex align-items-center">
-
-                <strong>WebSocket - CSWSH</strong>
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        </div>
+            <strong><?php if($_SESSION['is_admin']){ echo "Welcome Admin!";}else{ echo "WebSocket";}?></strong>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
     </div>
-<?php } ?>
+    <a href="logout.php" class="navbar-brand d-flex align-items-center">LogOut</a>
+
+
+</div>
 <div class="collapse bg-dark" id="navbarHeader">
     <div class="container">
         <div class="row">
