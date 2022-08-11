d5fb6ff6049a38a0274d4b516984dab961ba8de6
Serhat ÇİÇEK <serhatdbs@outlook.com>
2022-08-11 17:54:24 +0300

    change index.php

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ if (!isset($_SESSION['userId'])) {
             <div class="container">
 
                 <div class="row">
-                    <div class="col-md-4">
+                    <div class="col-md-6">
                         <div class="card mb-4 box-shadow">
                             <div class="card-header">
                                 <h5>Cross-Site WebSocket Hijacking (CSWSH)</h5>
@@ -57,7 +57,7 @@ if (!isset($_SESSION['userId'])) {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-6">
                         <div class="card mb-4 box-shadow">
                             <div class="card-header">
                                 <h5>XSS</h5>
@@ -73,22 +73,7 @@ if (!isset($_SESSION['userId'])) {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-4">
-                        <div class="card mb-4 box-shadow">
-                            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-                            <div class="card-body">
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                                    </div>
-                                    <small class="text-muted">9 mins</small>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
+                    
 
                 </div>
             </div>
